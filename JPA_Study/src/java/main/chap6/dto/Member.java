f0ae63d5813a3fba18997ef48cc5d28a66aaf901
Kyunghun Jeon <penpen787@gmail.com>
2016-01-08 17:52:16 +0900

    p.206

---
+++ JPA_Study/src/java/main/chap6/dto/Member.java
@@ -0,0 +1,64 @@
+package chap6.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity(name = "chap6_member")
+@Table(name = "C6_MEMBER")
+public class Member {
+
+	@Id
+	private String id;
+	private String username;
+
+	
+	/**
+	 * 연관관계 매핑
+	 */
+	@ManyToOne
+	@JoinColumn(name = "TEAM_ID")
+	private Team team;
+
+	public Member() {}
+	
+	public Member(String id, String username) {
+		this.id = id;
+		this.username = username;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setTeam(Team team) {
+		
+		// if member already has team, remove this member from the team.
+		if(this.team != null) {
+			this.team.getMembers().remove(this);
+		}
+		
+		// make relation each direction
+		this.team = team;
+		team.getMembers().add(this);
+	}
+
+}
---
+++ JPA_Study/src/java/main/chap6/dto/Team.java
@@ -0,0 +1,56 @@
+package chap6.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity(name = "chap6_team")
+@Table(name = "C6_TEAM")
+public class Team {
+
+	@Id
+	private String id;
+	
+	private String name;
+	
+	// 'mappedBy' property indicates the owner of the relationship,
+	// in this case, it points Member.team
+	@OneToMany(mappedBy = "team")
+	private List<Member> members = new ArrayList<>();
+	
+	public Team() {}
+	
+	public Team(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Member> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<Member> members) {
+		this.members = members;
+	}
+
+}
